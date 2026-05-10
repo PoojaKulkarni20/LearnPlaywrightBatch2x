0c352bb0477f7c9766149bfb6ce82048dafa8e0d
VS Code User <user@example.com>
2026-05-10 16:44:53 +0530

    today's commit 10-05-26

--- 02_ JS.js
+++ 02_ JS.js
@@ -4,6 +4,9 @@ console.log(x);
 for (let i = 0; i < 100000; i++) 
     {
         console.log(i);
+        print();
     }   
-
+function print() {
+    console.log("hello world");
+}
 
---
+++ 05_JSexpample.js
@@ -0,0 +1,7 @@
+const add = (a, b) => a + b;
+
+const num1 = 5;
+const num2 = 7;
+const sum = add(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
