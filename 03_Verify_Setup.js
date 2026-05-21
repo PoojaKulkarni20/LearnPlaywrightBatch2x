00e0ff0aefe2e1476cdf7a3c955075ff9f3aab0d
Pooja Kulkarni <poojak201197@gmail.com>
2026-05-21 19:57:12 +0530

    Latest code of PlayWright Batch

--- 03_Verify_Setup.js
+++
@@ -1,3 +0,0 @@
-console.log(process.platform);
-console.log(process.version);
-console.log(process.arch);
--- 05_JSexpample.js
+++
@@ -1,7 +0,0 @@
-const add = (a, b) => a + b;
-
-const num1 = 5;
-const num2 = 7;
-const sum = add(num1, num2);
-//New line added for testing
-console.log(`The sum of ${num1} and ${num2} is ${sum}.`);
